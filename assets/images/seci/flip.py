2408d9dae8913a3781a3fd29af382da6c12c0a1b
Arpit Kaithwar <arpitkaithwar02@gmail.com>
2022-12-20 13:17:44 +0530

    Team Section Updated

--- assets/images/seci/flip.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageOps
-import sys
-
-im = Image.open(sys.argv[1])
-
-im_flip = ImageOps.flip(im)
-
-im_mirror = ImageOps.mirror(im)
-im_mirror.save(sys.argv[1], quality=95)
